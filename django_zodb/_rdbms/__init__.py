6ac272e3fee04e6a64741c1d0c488463d5a95f48
Osvaldo Santana Neto <github@osantana.me>
2010-05-06 15:06:39 -0300

    MySQL support (no tests yet)
      - Refactoring storage.py to make it easier to test
      - More RelStorageFactory code
      - Add MySQLStorageFactory code but there is no test yet
        (so... it's not done :D)

--- django_zodb/_rdbms/__init__.py
+++ django_zodb/_rdbms/__init__.py
@@ -13,48 +13,30 @@ from django_zodb.storage import StorageFactory
 from django_zodb.config import parse_bool, parse_tuple, IGNORE
 
 class RelStorageFactory(StorageFactory):
-    _args = (
-        ('name', str, 'name', None),
-        ('read_only', parse_bool, 'read_only', False),
-        ('blobstorage_dir', str, 'blob_dir', None),
-        ('poll_interval', int, 'poll_interval', 0),
-        ('keep_history', parse_bool, 'keep_history', True),
-        ('replica_conf', str, 'replica_conf', None),
-        ('replica_timeout', float, 'replica_timeout', 600.0),
-        ('pack_gc', parse_bool, 'pack_gc', True),
-        ('pack_dry_run', parse_bool, 'pack_dry_run', False),
-        ('pack_batch_timeout', float, 'pack_batch_timeout', 5.0),
-        ('pack_duty_cycle', float, 'pack_duty_cycle', 0.5),
-        ('pack_max_delay', float, 'pack_max_delay', 20.0),
-        ('cache_servers', parse_tuple, 'cache_servers', ()),
-        ('cache_module_name', str, 'cache_module_name', 'memcache'),
-        ('cache_prefix', str, 'cache_prefix', ''),
-        ('cache_local_mb', int, 'cache_local_mb', 10),
-        ('cache_delta_size_limit', int, 'cache_delta_size_limit', 10000),
-        ('commit_lock_timeout', int, 'commit_lock_timeout', 30),
-        ('commit_lock_id', int, 'commit_lock_id', 0),
+    _storage = RelStorage
+    _storage_args = (
+        ('name', str, 'name', IGNORE),
+        ('create', parse_bool, 'create', IGNORE),
+        ('read_only', parse_bool, 'read_only', IGNORE),
+        ('blobstorage_dir', str, 'blob_dir', IGNORE),
+        ('poll_interval', int, 'poll_interval', IGNORE),
+        ('keep_history', parse_bool, 'keep_history', IGNORE),
+        ('replica_conf', str, 'replica_conf', IGNORE),
+        ('replica_timeout', float, 'replica_timeout', IGNORE),
+        ('pack_gc', parse_bool, 'pack_gc', IGNORE),
+        ('pack_dry_run', parse_bool, 'pack_dry_run', IGNORE),
+        ('pack_batch_timeout', float, 'pack_batch_timeout', IGNORE),
+        ('pack_duty_cycle', float, 'pack_duty_cycle', IGNORE),
+        ('pack_max_delay', float, 'pack_max_delay', IGNORE),
+        ('cache_servers', parse_tuple, 'cache_servers', IGNORE),
+        ('cache_module_name', str, 'cache_module_name', IGNORE),
+        ('cache_prefix', str, 'cache_prefix', IGNORE),
+        ('cache_local_mb', int, 'cache_local_mb', IGNORE),
+        ('cache_delta_size_limit', int, 'cache_delta_size_limit', IGNORE),
+        # ('cache', str, 'cache', None), # XXX: Is there an alternative to default relstorage.cache.StorageCache?
     )
 
-    def get_adapter(self):
-        pass
-
-    def get_base_storage(self, **kwargs): # receive _args
-        adapter = self.get_adapter()
-        return RelStorage(adapter, **kwargs)
-
-# import relstorage
-# from relstorage.relstorage import RelStorage
-# kwds = args_to_kwds(args)
-# if args['db_type'] == 'mysql':
-#     from relstorage.adapters.mysql import MySQLAdapter
-#     adapter = MySQLAdapter(**kwds)
-# elif args['db_type'] == 'postgresql':
-#     from relstorage.adapters.postgresql import PostgreSQLAdapter
-#     adapter = PostgreSQLAdapter(**kwds)
-# elif args['db_type'] == 'oracle':
-#     from relstorage.adapters.oracle import OracleAdapter
-#     adapter = OracleAdapter(**kwds)
-# else:
-#     err_msg = 'unknown database type: %s' % args['db_type']
-#     raise NotImplementedError, err_msg
-# storage = RelStorage(adapter)
+    def get_base_storage(self, **options): # receive self._args
+        create = options.pop("create")
+        adapter = self.get_adapter(options)
+        return self._storage(adapter, **options)
